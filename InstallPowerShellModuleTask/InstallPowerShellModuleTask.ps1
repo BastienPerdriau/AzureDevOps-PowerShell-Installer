b9bc8eb72aecb83247baf80dbb5f320a5053ed27
Bastien Perdriau <bastien.perdriau@live.fr>
2019-04-23 23:41:11 +0200

    First version of the task

---
+++ InstallPowerShellModuleTask/InstallPowerShellModuleTask.ps1
@@ -0,0 +1,18 @@
+Trace-VstsEnteringInvocation $MyInvocation
+
+[string]$ModuleName = Get-VstsInput -Name moduleName -Require
+
+$modules = $ModuleName.Split(",",[StringSplitOptions]'RemoveEmptyEntries')
+
+foreach ($module in $modules)
+{
+    $module = $module.Trim()
+
+    if ((Get-module $module -ListAvailable).Count -lt 1)
+    {
+        Install-PackageProvider -Name NuGet -Force -Scope CurrentUser | Out-Null
+        Install-Module -Name $module -Force -Scope CurrentUser | Out-Null
+    }
+}
+
+Trace-VstsLeavingInvocation $MyInvocation
